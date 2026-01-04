0083f7d9b37b58544ec3a3a4e223a0a15e3e3c0e
Wolf Mermelstein <wolf@404wolf.com>
2026-01-04 13:37:16 -0500

    add static build with release

--- nix/build.nix
+++ nix/build.nix
@@ -1,20 +1,21 @@
 { lib, pkgs }:
+ pkgs.pkgsCross.musl64.rustPlatform.buildRustPackage {
+   pname = "mdvalidate";
+   version = "0.1.0";
 
-pkgs.pkgsCross.musl64.rustPlatform.buildRustPackage {
-  pname = "mdvalidate";
-  version = "0.1.0";
+   src = ../.;
 
-  src = ../.;
+   cargoHash = "sha256-HCxLc3M3Tza1pVlLOPgmiZu6Ra2oFzSCmwSSTuQW+u0=";
 
-  cargoHash = "sha256-HCxLc3M3Tza1pVlLOPgmiZu6Ra2oFzSCmwSSTuQW+u0=";
+   env = {
+     RUSTFLAGS = "-C target-feature=+crt-static -C link-self-contained=yes";
+   };
 
-  env = {
-    RUSTFLAGS = "-C target-feature=+crt-static";
-  };
+   CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
 
-  meta = {
-    description = "Markdown Schema validator";
-    homepage = "https://github.com/404Wolf/mdvalidate";
-    license = lib.licenses.mit;
-  };
-}
+   meta = {
+     description = "Markdown Schema validator";
+     homepage = "https://github.com/404Wolf/mdvalidate";
+     license = lib.licenses.mit;
+   };
+ }
