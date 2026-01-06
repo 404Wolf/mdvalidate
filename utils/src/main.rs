56bba69b637d1a8ec53adbba31ce506bfc91b848
Wolf Mermelstein <wolf@404wolf.com>
2026-01-06 13:24:33 -0500

    add utils crate

---
+++ utils/src/main.rs
@@ -0,0 +1,34 @@
+mod node_print;
+
+use std::env;
+use std::fs;
+use std::io::{self, Read};
+
+use node_print::PrettyPrint;
+use tree_sitter::Parser;
+use tree_sitter_markdown::language;
+
+fn read_input() -> io::Result<String> {
+    let mut args = env::args().skip(1);
+    if let Some(path) = args.next() {
+        fs::read_to_string(path)
+    } else {
+        let mut input = String::new();
+        io::stdin().read_to_string(&mut input)?;
+        Ok(input)
+    }
+}
+
+fn main() {
+    let input = read_input().expect("Failed to read input");
+
+    let mut parser = Parser::new();
+    parser
+        .set_language(language())
+        .expect("Failed to load Markdown language");
+
+    let tree = parser.parse(&input, None).expect("Failed to parse");
+    let node = tree.root_node();
+
+    print!("{}", node.pretty_print());
+}
